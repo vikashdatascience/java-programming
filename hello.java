a780891678370706f47cfacfedfc972a0cb9ca0f
Vikash <vikashvm2008@gmail.com>
2026-07-22 18:09:58 +0530

    wed

--- hello.java
+++ hello.java
@@ -1,42 +1,19 @@
 import java.util.Scanner;
 
-class Movie {
-
-    void recommend(String genre) {
-        if (genre.equals("ACTION")){
-            System.out.println("Recommended: LEO");
-            System.out.println("Recommended: VIKRAM");
-            System.out.println("Recommended: COOLIE");
-        }
-        else if (genre.equals("COMEDY")){
-            System.out.println("recommended: JAILER");
-            System.out.println("Recommended: FRIENDS");
-            System.out.println("Recommended: DD RETURNS");
-        }
-        else if (genre.equals("FEEL GOOD")){
-            System.out.println("Recommended: 96");
-            System.out.println("Recommended: MEIYAZHAGAN");
-            System.out.println("Recommended: GOOD NIGHT");
-        }
-        else if (genre.equals("ADVENTURE"))
-        {
-            System.out.println("Recommended: PULI");
-            System.out.println("Recommended: PIRATES OF THE CARIBBEAN");
-            System.out.println("Recommended: JUMANJI");
-
-        }    
-    }
-}
-
 public class hello {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for(int i = 1; i<=3; i++)
+        {
+            String s = sc.next();
+            int n = sc.nextInt();
+            System.out.printf("%-15s%03d%n", s, n);
 
-        Movie m = new Movie();
-
-        System.out.print("Enter Genre: ");
-        String genre = sc.nextLine();
-
-        m.recommend(genre);
+            
+           
     }
+    System.out.println("================================");
+}
 }
--- introduction.java
+++ introduction.java
@@ -604,7 +604,26 @@ public class hello {
 }
 ----------------------------------------------------------------------------------------------------
 
+import java.util.Scanner;
 
+public class Solution {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for(int i = 1; i<=3; i++)
+        {
+            String s = sc.next();
+            int n = sc.nextInt();
+            System.out.printf("%-15s%03d%n", s, n);
+
+            
+           
+    }
+    System.out.println("================================");
+}
+}
+----------------------------------------------------------------------------------------------------
 
 
 
