168a10c1fb106b5392bbffb00570a2d2ab63e12f
Vikash <vikashvm2008@gmail.com>
2026-07-24 14:04:19 +0530

    commited

--- hello.java
+++ hello.java
@@ -1,25 +1,52 @@
-
-
-
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 import java.util.Scanner;
 
+class Movie {
+
+    void action() {
+        System.out.println("LEO");
+        System.out.println("VIKRAM");
+        System.out.println("COOLIE");
+    }
+
+    void comedy() {
+        System.out.println("JAILER");
+        System.out.println("FRIENDS");
+        System.out.println("DD RETURNS");
+    }
+
+    void feelGood() {
+        System.out.println("96");
+        System.out.println("MEIYAZHAGAN");
+        System.out.println("GOOD NIGHT");
+    }
+
+    void adventure() {
+        System.out.println("PULI");
+        System.out.println("PIRATES OF THE CARIBBEAN");
+        System.out.println("JUMANJI");
+    }
+}
+
 public class hello {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt();
-        
-        for(int i = 1; i<=10; i++)
-        {
-            System.out.println(N + " x " + i + " = " + (N*i));
-        }
+        Movie m = new Movie();
+
+        System.out.print("Enter Genre: ");
+        String genre = sc.nextLine();
+
+        if (genre.equalsIgnoreCase("ACTION"))
+            m.action();
+        else if (genre.equalsIgnoreCase("COMEDY"))
+            m.comedy();
+        else if (genre.equalsIgnoreCase("FEEL GOOD"))
+            m.feelGood();
+        else if (genre.equalsIgnoreCase("ADVENTURE"))
+            m.adventure();
+        else
+            System.out.println("Invalid Genre");
+
+        sc.close();
     }
 }
--- introduction.java
+++ introduction.java
@@ -720,6 +720,62 @@ public class solution{
 -------------------------------------------------------------------------------------------------
 
 
+import java.util.Scanner;
+
+class Movie {
+
+    void action() {
+        System.out.println("LEO");
+        System.out.println("VIKRAM");
+        System.out.println("COOLIE");
+    }
+
+    void comedy() {
+        System.out.println("JAILER");
+        System.out.println("FRIENDS");
+        System.out.println("DD RETURNS");
+    }
+
+    void feelGood() {
+        System.out.println("96");
+        System.out.println("MEIYAZHAGAN");
+        System.out.println("GOOD NIGHT");
+    }
+
+    void adventure() {
+        System.out.println("PULI");
+        System.out.println("PIRATES OF THE CARIBBEAN");
+        System.out.println("JUMANJI");
+    }
+}
+
+public class hello {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Movie m = new Movie();
+
+        System.out.print("Enter Genre: ");
+        String genre = sc.nextLine();
+
+        if (genre.equalsIgnoreCase("ACTION"))
+            m.action();
+        else if (genre.equalsIgnoreCase("COMEDY"))
+            m.comedy();
+        else if (genre.equalsIgnoreCase("FEEL GOOD"))
+            m.feelGood();
+        else if (genre.equalsIgnoreCase("ADVENTURE"))
+            m.adventure();
+        else
+            System.out.println("Invalid Genre");
+
+        sc.close();
+    }
+}
+
+-------------------------------------------------------------------------------------------------------
+
+
 
 
 
